9d73ecfd107e79fcd7ac494a112d61c9df048095
CSaintos <48599958+CSaintos@users.noreply.github.com>
2024-09-07 13:57:34 -0700

    Fixed Definer.hpp and Semanticizer.hpp changes from Stage

--- stem/inc/front/Definer.hpp
+++ stem/inc/front/Definer.hpp
@@ -7,10 +7,23 @@
 
 #pragma once
 
+#include <memory>
+
+#include "Node.hpp"
+
 namespace stem
 {
   class Definer
   {
-
+  private:
+  public:
+    Definer();
+    /*
+     * @brief analyzes parse tree to append to symbol table and modify tree accordingly
+     *
+     * @param root node of parse tree to analyze
+     */
+    void define(std::unique_ptr<stem::Node>& root);
+    void getSymbolTable();
   };
 }
