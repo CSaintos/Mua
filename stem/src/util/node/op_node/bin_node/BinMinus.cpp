6845293018df22efc261f6911eac3ca3ac135b16
CSaintos <48599958+CSaintos@users.noreply.github.com>
2024-10-01 20:09:23 -0700

    BinMinus can now create negative digits

--- stem/src/util/node/op_node/bin_node/BinMinus.cpp
+++ stem/src/util/node/op_node/bin_node/BinMinus.cpp
@@ -22,9 +22,17 @@ void BinMinus::interpret()
     if (node_left->tok.type == TokenType::DIGIT &&
       node_right->tok.type == TokenType::DIGIT)
     {
+      unique_ptr<Node> res_node;
       double lhs = std::stod(node_left->tok.lexemes);
       double rhs = std::stod(node_right->tok.lexemes);
       double result = lhs - rhs;
+      bool is_negative = false;
+
+      if (result < 0)
+      {
+        result = result * -1;
+        is_negative = true;
+      }
 
       string result_str = NumberUtils::stripTrailingZeros(std::to_string(result));
 
@@ -32,7 +40,14 @@ void BinMinus::interpret()
       res_tok.lexemes = result_str;
       res_tok.type = TokenType::DIGIT;
 
-      unique_ptr<Node> res_node = std::make_unique<ValueNode>(res_tok);
+      res_node = std::make_unique<ValueNode>(res_tok);
+
+      if (is_negative)
+      {
+        Token minus_tok;
+        minus_tok.type = TokenType::MINUS;
+        res_node = std::make_unique<UnaOpNode>(minus_tok, res_node);
+      }
 
       NodeUtils::replaceNode(this, res_node);
     }
