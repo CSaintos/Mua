49239b832c0a77f102cf8ea08078fc836add072a
CSaintos <48599958+CSaintos@users.noreply.github.com>
2024-10-24 07:06:13 -0700

    Add support for signed consecutive division w/ whole numbers

--- stem/src/util/node/op_node/bin_node/FSlash.cpp
+++ stem/src/util/node/op_node/bin_node/FSlash.cpp
@@ -39,168 +39,196 @@ bool FSlash::interpret(const unordered_set<InterpretType> &flags)
   {
     return change;
   }
-  if (!(is_left_leaf && is_right_leaf)) // not the case: both are leaves
+  unique_ptr<Node> lhs_node;
+  unique_ptr<Node> rhs_node;
+  unique_ptr<Node> left_numerator;
+  unique_ptr<Node> left_denominator;
+  unique_ptr<Node> right_numerator;
+  unique_ptr<Node> right_denominator;
+  bool is_left_minus = false;
+  bool is_right_minus = false;
+  bool is_minus = false;
+  bool removable_right_paren = false;
+
+  if (!is_left_leaf)
   {
-    if (!(is_left_leaf || is_right_leaf)) // not even the case: either one is a leaf
+    if (node_left->tok.type == TokenType::MINUS)
     {
-      if (node_left->tok.type == TokenType::FSLASH && node_right->tok.type == TokenType::FSLASH)
+      UnaOpNode* una_op_node = static_cast<UnaOpNode*>(node_left.get());
+      is_left_minus = true;
+
+      if (una_op_node->node->tok.type == TokenType::DIGIT)
       {
-        cout << "Not implemented yet (1)" << endl;
-      }
-      else 
-      {
-        cout << "Not implemented yet (2)" << endl;  
+        lhs_node = NumberUtils::fractionalize(una_op_node->node->to_repr());
       }
     }
-    else 
+    else if (node_left->tok.type == TokenType::FSLASH)
     {
-      if (!is_left_leaf)
-      {
-        if (node_left->tok.type == TokenType::FSLASH)
-        {
-          unique_ptr<Node> left_numerator;
-          unique_ptr<Node> left_denominator;
-          unique_ptr<Node> right_denominator;
-          BinOpNode* bin_op_node = static_cast<BinOpNode*>(node_left.get());
-          left_numerator = std::move(bin_op_node->node_left);
-          node_left = std::move(left_numerator);
-          node_left->parent = this;
-          left_denominator = std::move(bin_op_node->node_right);
-          right_denominator = std::move(node_right);
-          Token tok_op;
-          tok_op.type = TokenType::ASTERISK;
-          unique_ptr<Node> denominator = std::make_unique<Asterisk>(left_denominator, tok_op, right_denominator);
-          Token tok_paren;
-          tok_paren.type = TokenType::LPAREN;
-          unique_ptr<Node> paren = std::make_unique<Paren>(tok_paren, denominator);
-          node_right = std::move(paren);
-          node_right->parent = this;
-          change = true;
-        }
-        else if (node_left->tok.type == TokenType::MINUS)
-        {
-          unique_ptr<Node> res_node;
-          unique_ptr<Node> lhs_node;
-          unique_ptr<Node> rhs_node;
-
-          UnaOpNode* una_op_node = static_cast<UnaOpNode*>(node_left.get());
-
-          if (una_op_node->node->tok.type == TokenType::DIGIT && node_right->tok.type == TokenType::DIGIT)
-          {
-            lhs_node = NumberUtils::fractionalize(una_op_node->node->to_repr());
-            rhs_node = NumberUtils::fractionalize(node_right->to_repr());
-
-            if (lhs_node->isLeaf() && rhs_node->isLeaf())
-            {
-              int lhs = std::stod(lhs_node->to_repr());
-              int rhs = std::stod(rhs_node->to_repr());
-              int gcd = std::gcd(lhs, rhs);
-
-              if (gcd == 1)
-              {
-                if (flags.count(InterpretType::DECIMALIZE) > 0)
-                {
-                  double result = double(lhs) / double(rhs);
-                  string result_str = NumberUtils::stripTrailingZeros(std::to_string(result));
-                  Token res_tok;
-                  res_tok.type = TokenType::DIGIT;
-                  res_tok.lexemes = result_str;
-                  Token minus_tok;
-                  minus_tok.type = TokenType::MINUS;
-                  res_node = std::make_unique<ValueNode>(res_tok);
-                  res_node = std::make_unique<UnaMinus>(minus_tok, res_node);
-                  NodeUtils::replaceNode(this, res_node);
-                  change = true;
-                }
-              }
-              else
-              {
-                lhs = lhs / gcd;
-                rhs = rhs / gcd;
-                lhs_node->tok.lexemes = std::to_string(lhs);
-                rhs_node->tok.lexemes = std::to_string(rhs);
-                una_op_node->node = std::move(lhs_node);
-                una_op_node->node->parent = una_op_node;
-                node_right = std::move(rhs_node);
-                node_right->parent = this;
-                change = true;
-              }
-            }
-          }
-          else
-          {
-            cout << "Not implemented yet (5)" << endl;
-          }
-        }
-        else
-        {
-          cout << "Not implemented yet (3)" << endl;
-        }
-      }
-      else
-      {
-        cout << "Not implemented yet (4)" << endl;
-      }
+      BinOpNode* bin_op_node = static_cast<BinOpNode*>(node_left.get());
+      left_numerator = std::move(bin_op_node->node_left);
+      left_denominator = std::move(bin_op_node->node_right);
+    }
+    else
+    {
+      cout << "Not implemented yet (2)";
     }
   }
   else
   {
-    unique_ptr<Node> res_node;
-    unique_ptr<Node> lhs_node;
-    unique_ptr<Node> rhs_node;
-
-    if (node_left->tok.type == TokenType::DIGIT &&
-    node_right->tok.type == TokenType::DIGIT)
+    if (node_left->tok.type == TokenType::DIGIT)
     {
       lhs_node = NumberUtils::fractionalize(node_left->to_repr());
-      rhs_node = NumberUtils::fractionalize(node_right->to_repr());
+    }
+  }
 
-      if (lhs_node->isLeaf() && rhs_node->isLeaf())
+  if (!is_right_leaf)
+  {
+    if (node_right->tok.type == TokenType::MINUS)
+    {
+      UnaOpNode* una_op_node = static_cast<UnaOpNode*>(node_right.get());
+      is_right_minus = true;
+
+      if (una_op_node->node->tok.type == TokenType::DIGIT)
       {
-        int lhs = std::stod(lhs_node->to_repr());
-        int rhs = std::stod(rhs_node->to_repr());
-        int gcd = std::gcd(lhs, rhs);
-    
-        if (gcd == 1)
+        rhs_node = NumberUtils::fractionalize(una_op_node->node->to_repr());
+      }
+    }
+    else if (node_right->tok.type == TokenType::FSLASH)
+    {
+      cout << "Not implemented yet (1)" << endl;
+    }
+    else if (node_right->tok.type == TokenType::LPAREN)
+    {
+      UnaOpNode* una_op_node = static_cast<UnaOpNode*>(node_right.get());
+          
+      if (una_op_node->node->tok.type == TokenType::MINUS)
+      {
+        is_right_minus = true;
+        una_op_node = static_cast<UnaOpNode*>(una_op_node->node.get());
+      }
+      if (una_op_node->node->tok.type == TokenType::DIGIT)
+      {
+        removable_right_paren = true;
+        rhs_node = NumberUtils::fractionalize(una_op_node->node->to_repr());
+      }
+    }
+    else
+    {
+      cout << "Not implemented yet (2)" << endl;
+    }
+  }
+  else
+  {
+    if (node_right->tok.type == TokenType::DIGIT)
+    {
+      rhs_node = NumberUtils::fractionalize(node_right->to_repr());
+    }
+  }
+      
+  is_minus = (
+    (is_left_minus || 
+    is_right_minus) &&
+    !(is_left_minus &&
+    is_right_minus)
+  );
+  if (lhs_node != nullptr && rhs_node != nullptr)
+  {
+    if (lhs_node->isLeaf() && rhs_node->isLeaf())
+    {
+      int lhs = std::stod(lhs_node->to_repr());
+      int rhs = std::stod(rhs_node->to_repr());
+      int gcd = std::gcd(lhs, rhs);
+
+      if (gcd == 1)
+      {
+        if (flags.count(InterpretType::DECIMALIZE) > 0)
         {
-          if (flags.count(InterpretType::DECIMALIZE) > 0)
+          double result = double(lhs) / double(rhs);
+          string result_str = NumberUtils::stripTrailingZeros(std::to_string(result));
+          Token res_tok;
+          res_tok.type = TokenType::DIGIT;
+          res_tok.lexemes = result_str;
+          lhs_node = std::make_unique<ValueNode>(res_tok);
+          if (is_minus)
           {
-            double result = double(lhs) / double(rhs);
-            string result_str = NumberUtils::stripTrailingZeros(std::to_string(result));
-            Token res_tok;
-            res_tok.type = TokenType::DIGIT;
-            res_tok.lexemes = result_str;
-            res_node = std::make_unique<ValueNode>(res_tok);
-            NodeUtils::replaceNode(this, res_node);
-            change = true;
+            Token minus_tok;
+            minus_tok.type = TokenType::MINUS;
+            lhs_node = std::make_unique<UnaMinus>(minus_tok, lhs_node);
           }
+          NodeUtils::replaceNode(this, lhs_node);
+          change = true;
         }
-        else
+        else if (rhs == 1)
         {
-          lhs = lhs / gcd;
-          rhs = rhs / gcd;
-          lhs_node->tok.lexemes = std::to_string(lhs);
-          rhs_node->tok.lexemes = std::to_string(rhs);
-          node_left = std::move(lhs_node);
-          node_left->parent = this;
-          node_right = std::move(rhs_node);
-          node_right->parent = this;
+          if (is_minus)
+          {
+            Token minus_tok;
+            minus_tok.type = TokenType::MINUS;
+            lhs_node = std::make_unique<UnaMinus>(minus_tok, lhs_node);
+          }
+          NodeUtils::replaceNode(this, lhs_node);
+          change = true;
+        }
+        else if (removable_right_paren)
+        {
+          if (is_right_minus)
+          {
+            Token minus_tok;
+            minus_tok.type = TokenType::MINUS;
+            rhs_node = std::make_unique<UnaMinus>(minus_tok, rhs_node);
+          }
+          NodeUtils::replaceNode(node_right.get(), rhs_node);
           change = true;
         }
       }
       else
       {
-        if (!lhs_node->isLeaf())
+        lhs = lhs / gcd;
+        rhs = rhs / gcd;
+        lhs_node->tok.lexemes = std::to_string(lhs);
+        if (is_minus)
         {
-          node_left = std::move(lhs_node);
+          Token minus_tok;
+          minus_tok.type = TokenType::MINUS;
+          lhs_node = std::make_unique<UnaMinus>(minus_tok, lhs_node);
         }
-        if (!rhs_node->isLeaf())
+        if (rhs != 1)
         {
-          node_right = std::move(rhs_node);
+          rhs_node->tok.lexemes = std::to_string(rhs);
+          NodeUtils::replaceNode(node_left.get(), lhs_node);
+          NodeUtils::replaceNode(node_right.get(), rhs_node);
+        }
+        else
+        {
+          NodeUtils::replaceNode(this, lhs_node);
         }
         change = true;
       }
     }
+    else
+    {
+      if (!lhs_node->isLeaf())
+      {
+        NodeUtils::replaceNode(node_left.get(), lhs_node);
+      }
+      if (!rhs_node->isLeaf())
+      {
+        NodeUtils::replaceNode(node_right.get(), rhs_node);
+      }
+      change = true;
+    }
+  }
+  else if (left_numerator != nullptr && left_denominator != nullptr && rhs_node != nullptr)
+  {
+    Token tok_op;
+    tok_op.type = TokenType::ASTERISK;
+    unique_ptr<Node> denominator = std::make_unique<Asterisk>(left_denominator, tok_op, rhs_node);
+    Token tok_paren;
+    tok_paren.type = TokenType::LPAREN;
+    unique_ptr<Node> paren = std::make_unique<Paren>(tok_paren, denominator);
+    NodeUtils::replaceNode(node_left.get(), left_numerator);
+    NodeUtils::replaceNode(node_right.get(), paren);
+    change = true;
   }
 
   return change;
