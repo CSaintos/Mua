bbe26af48a83e65c26632bd494a611b208be36b1
CSaintos <48599958+CSaintos@users.noreply.github.com>
2025-06-09 19:15:44 -0700

    Modify css for route.tsx

--- docs/src/routes/route.tsx
+++ docs/src/routes/route.tsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import { 
   createFileRoute,
 } from '@tanstack/react-router'
 import {
   Grid,
+  InputBase,
+  Typography
 } from '@mui/material';
 
 export const Route = createFileRoute('/')({
@@ -10,11 +13,42 @@ export const Route = createFileRoute('/')({
 })
 
 function Index() {
+  const [muaCode, setMuaCode] = useState('') 
+  const rows = 28
   return <>
     <Grid container columns={2} spacing={1} sx={{padding:'0.5rem', minHeight:'93vh'}}>
-      <Grid size={{xs:2, sm:1}} sx={{backgroundColor:'orange'}}>
+      <Grid 
+      size={{xs:2, sm:1}} 
+      sx={{
+        backgroundColor:'#777',
+        padding:'0.5rem',
+        borderRadius:'3%',
+      }}>
+        <InputBase
+        fullWidth
+        multiline
+        minRows={rows}
+        maxRows={rows}
+        autoFocus={true}
+        placeholder='1+1;'
+        onChange={(event) => {
+          setMuaCode(event.target.value)
+        }}
+        sx={{
+          borderWidth:'0',
+        }}
+        />
       </Grid>
-      <Grid size={{xs:2, sm:1}} sx={{backgroundColor:'green'}}>
+      <Grid 
+      size={{xs:2, sm:1}} 
+      sx={{
+        backgroundColor:'#999',
+        padding:'0.5rem',
+        borderRadius:'3%'
+      }}>
+        <Typography>
+          {muaCode}
+        </Typography>
       </Grid>
     </Grid>
   </>
